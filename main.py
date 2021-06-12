b3efcd940de257974d26a705961f73bdb0574781
Alex Norell <3845666+alexnorell@users.noreply.github.com>
2021-06-12 14:23:13 -0700

    Naive implementation

--- main.py
+++ main.py
@@ -1,15 +1,28 @@
 from typing import Optional
 
 from fastapi import FastAPI
-
+from datetime import date, datetime
 app = FastAPI()
 
+users = []
+yos = {}
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+@app.post("/create_user/")
+def create_user(username: str):
+    users.append(username)
+    yos[username] = []
+    return "User Created"
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Optional[str] = None):
-    return {"item_id": item_id, "q": q}
+@app.post("/yos/")
+def send_yo(from_user: str, to_user: str):
+    yo_to_send = (from_user, datetime.now())
+    yos[to_user].append(yo_to_send)
+    return yo_to_send
+
+
+@app.get("/yos/")
+def get_yos(user: str):
+    to_return = yos[user]
+    yos[user] = []
+    return to_return
