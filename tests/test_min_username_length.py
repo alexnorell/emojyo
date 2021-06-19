c7cc4fcf3864041ed0454501586bbd16be0ef13f
Bryan Cole <cole.bryan.p@gmail.com>
2021-06-19 22:27:29 +0000

    Closes #11 adds and tests minimum username length

---
+++ tests/test_min_username_length.py
@@ -0,0 +1,24 @@
+import os
+import re
+
+import pytest
+from redis import Redis
+
+from emojyo.main import create_user, get_yos, send_yo
+from emojyo.data import Database
+
+DB = Database()
+
+@pytest.fixture
+def clean_redis():
+    Redis(host=os.environ["REDIS_HOST"], port=os.environ["REDIS_PORT"]).flushall()
+    yield
+    Redis(host=os.environ["REDIS_HOST"], port=os.environ["REDIS_PORT"]).flushall()
+
+
+def test_min_username_length_with_redis_running(clean_redis):
+    received_yos = {}
+    assert create_user('') != "User Created"
+    assert create_user('a') != "User Created"
+    assert create_user('ab') != "User Created"
+    assert create_user('abc') == "User Created"
